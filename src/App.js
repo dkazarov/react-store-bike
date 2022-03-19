8a8cf2d4b1627ae6e3f23ba9a839af035603a3a3
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 14:07:53 +0300

    add header html markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 function App() {
   return (
     <div className="wrapper">
-
+      <header>
+        <div className = "header__left">
+        <svg>
+          <div className="heder__left-info">
+            <h3>REACT SNEAKERS</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </svg>
+        </div>
+        <ul className="">
+          <li>
+            <svg />
+              <span>1205 грн</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
