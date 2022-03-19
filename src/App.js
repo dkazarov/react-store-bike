2d5d5171b58dde007a50092270743faa34e45903
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 13:51:29 +0300

    add font roboto & clear base html file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className = "header__left">
-          <img width={40} height={40} src="/img/logo.png" alt="logo icon" />
-          <div className="heder__left-info">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center">
+        <div className = "d-flex align-center">
+          <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="logo icon" />
+          <div className="header__left-info ">
             <h3>REACT SNEAKERS</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="">
-          <li>
+        <ul className="d-flex">
+          <li className="mr-30">
           <img width={18} height={18} src="/img/cart.svg" alt="cart icon" />
               <span>1205 грн</span>
           </li>
