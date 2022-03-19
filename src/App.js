4ee0863bd317b9f9fd9bb0ebcd97dcd6ad73e163
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 13:32:32 +0300

    finish refactoring

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import React from "react";
 function App() {
   return (
     <div className="container">
-      <header className="heagitder">
+      <header className="header">
           <div className="logo__inner">
-            <img className="logo__img" src="/img/logo.png" alt="logo icon" />
-            <h2>REACT SNEAKERS</h2>
-            <h5>Магазин лучших кроссовок</h5>
+            <div className="logo__img-inner">
+              <img className="logo__img" src="/img/logo.png" alt="logo icon" />
+            </div>
+            <div className="logo__text">
+              <h2 className="logo__text-title">REACT SNEAKERS</h2>
+              <h5>Магазин лучших кроссовок</h5>
+            </div>
           </div>
 
           <div className="header__action">
-            <img src="/img/card.svg" alt="card icon" />
-            <p>1200 грн</p>
-            <img src="/img/heart.svg" alt="heart icon" />
-            <img src="/img/user.svg" alt="user icon" />
+            <div>
+            <img className="header__action-cart" src="/img/cart.svg" alt="card icon" />
+            <span>1200 грн</span>
+            <img className="header__action-favorite" src="/img/heart.svg" alt="heart icon" />
+            <img className="header__action-user" src="/img/user.svg" alt="user icon" />
+            </div>
           </div>
       </header>
+      <main className="main">
+        <h1 className="main__title">Все кроссовки</h1>
+        <div className="card">
+          <img className="card__img" width={133} height={112} src="/img/sneakers/1.jpg" alt="product icon" />
+          <p className="card__descr">Мужские Кроссовки Nike Blazer Mid Suede</p>
+          <div className="card__price">
+              <p className="card__price-subtitle">Цена:</p>
+              <p className="card__price-price">1200 грн</p>
+          </div>
+            <button className="card__price-btn">Купить</button>
+        </div>
+      </main>
     </div>  
   );
 }
