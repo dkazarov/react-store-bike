5ddea0010dba8282e7a2467c3884aa21631e32e1
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 13:31:22 +0300

    add macro-css library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from "react";
+
 function App() {
   return (
     <div className="wrapper">
       <header>
         <div className = "header__left">
-        <svg>
+          <img width={40} height={40} src="/img/logo.png" alt="logo icon" />
           <div className="heder__left-info">
             <h3>REACT SNEAKERS</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
-        </svg>
         </div>
         <ul className="">
           <li>
-            <svg />
+          <img width={18} height={18} src="/img/cart.svg" alt="cart icon" />
               <span>1205 грн</span>
           </li>
           <li>
-            <svg />
+          <img width={18} height={18} src="/img/user.svg" alt="user icon" />
           </li>
         </ul>
       </header>
