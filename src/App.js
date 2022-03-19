3837034387fe32a798f93bc6dbf5568c96634b1d
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 14:07:02 +0300

    refactoring all html & scss

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@ import React from "react";
 
 function App() {
   return (
-    <div className="wrapper clear">
-      <header className="d-flex justify-between align-center">
-        <div className = "d-flex align-center">
-          <img className="m-15" width={40} height={40} src="/img/logo.png" alt="logo icon" />
-          <div className="header__left-info ">
-            <h3 className="text-uppercase">REACT SNEAKERS</h3>
-            <p>Магазин лучших кроссовок</p>
+    <div className="container">
+      <header className="heagitder">
+          <div className="logo__inner">
+            <img className="logo__img" src="/img/logo.png" alt="logo icon" />
+            <h2>REACT SNEAKERS</h2>
+            <h5>Магазин лучших кроссовок</h5>
           </div>
-        </div>
-        <ul className="d-flex m-15">
-          <li className="mr-20">
-          <img className="mr-10" width={18} height={18} src="/img/cart.svg" alt="cart icon" />
-              <span>1205 грн</span>
-          </li>
-          <li>
-          <img width={18} height={18} src="/img/user.svg" alt="user icon" />
-          </li>
-        </ul>
+
+          <div className="header__action">
+            <img src="/img/card.svg" alt="card icon" />
+            <p>1200 грн</p>
+            <img src="/img/heart.svg" alt="heart icon" />
+            <img src="/img/user.svg" alt="user icon" />
+          </div>
       </header>
-      <div className="content p-40">
-        <h1>Все кроссовки</h1>
-        ...
-      </div>
-    </div>
+    </div>  
   );
 }
 
