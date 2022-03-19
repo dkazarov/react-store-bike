e0c6b487ebdf05f067cda01ad277f4e2c785a055
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-19 13:09:38 +0300

    finis styled header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
     <div className="wrapper clear">
       <header className="d-flex justify-between align-center">
         <div className = "d-flex align-center">
-          <img className="mr-15" width={40} height={40} src="/img/logo.png" alt="logo icon" />
+          <img className="m-15" width={40} height={40} src="/img/logo.png" alt="logo icon" />
           <div className="header__left-info ">
-            <h3>REACT SNEAKERS</h3>
+            <h3 className="text-uppercase">REACT SNEAKERS</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="d-flex">
-          <li className="mr-30">
-          <img width={18} height={18} src="/img/cart.svg" alt="cart icon" />
+        <ul className="d-flex m-15">
+          <li className="mr-20">
+          <img className="mr-10" width={18} height={18} src="/img/cart.svg" alt="cart icon" />
               <span>1205 грн</span>
           </li>
           <li>
@@ -21,6 +21,10 @@ function App() {
           </li>
         </ul>
       </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
     </div>
   );
 }
