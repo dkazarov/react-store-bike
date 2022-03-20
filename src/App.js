c7e27ac5cc32b09cd7c70b4557e4f9968f66b874
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-03-20 15:54:08 +0300

    add mini ratabase & deploy this to mockapAPI

--- src/App.js
+++ src/App.js
@@ -5,24 +5,44 @@ import Header from "./components/Header";
 
 const arr = [
   {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
-    imageUrl: "/img/sneakers/1.jpg",
+    "title": "Мужские Кроссовки Nike Blazer Mid Suede",
+    "price": 1299,
+    "imageUrl": "/img/sneakers/1.jpg"
   },
   {
-    title: "Мужские Кроссовки Nike Air Max 270",
-    price: 15600,
-    imageUrl: "/img/sneakers/2.jpg",
+    "title": "Мужские Кроссовки Nike Air Max 270",
+    "price": 1560,
+    "imageUrl": "/img/sneakers/2.jpg"
   },
   {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 8499,
-    imageUrl: "/img/sneakers/3.jpg",
+    "title": "Мужские Кроссовки Nike Blazer Mid Suede",
+    "price": 849,
+    "imageUrl": "/img/sneakers/3.jpg"
   },
   {
-    title: "Кроссовки Puma X Aka Boku Future Rider",
-    price: 8999,
-    imageUrl: "/img/sneakers/4.jpg",
+    "title": "Кроссовки Puma X Aka Boku Future Rider",
+    "price": 899,
+    "imageUrl": "/img/sneakers/4.jpg"
+  },
+  {
+    "title": "Мужские Кроссовки Under Armour Curry 8",
+    "price": 1519,
+    "imageUrl": "/img/sneakers/5.jpg"
+  },
+  {
+    "title": "Мужские Кроссовки Nike Kyrie 7",
+    "price": 899,
+    "imageUrl": "/img/sneakers/6.jpg"
+  },
+  {
+    "title": "Мужские Кроссовки Jordan Air Jordan 11",
+    "price": 899,
+    "imageUrl": "/img/sneakers/7.jpg"
+  },
+  {
+    "title": "Мужские Кроссовки Nike LeBron XVIII",
+    "price": 1649,
+    "imageUrl": "/img/sneakers/8 .jpg"
   },
 ];
 
